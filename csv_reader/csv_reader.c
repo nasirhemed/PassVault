8919ad0242545e828886f8a4cc1949b029c7e82f
Nasir Hemed <nasirhemed@ymail.com>
2018-08-23 13:34:03 -0400

    Add initial csv_reader commit Create a Makefile for csv_reader

---
+++ csv_reader/csv_reader.c
@@ -0,0 +1,87 @@
+/*
+ * CSV Reader: This program reads entries from a CSV file.
+ * The labels will be provided as stdin and the output will
+ * be printed to stdout
+ *
+ * Note: The CSV file is of the form Domain name, Username, Password
+ * Usage: csv_reader file
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAXCHARS 256 
+
+/**
+ * Given a char array and a CSV file pointer, set the string
+ * to the cell the file pointer is currently at
+ *
+ * Precondition: File is a CSV file that is comma seperated
+ */
+void set_string(char password[], FILE* file) {
+	char *str = password;
+	int i = 0;
+	int c;
+	while ((c = fgetc(file)) != '\n' && c != EOF && c != ',') {
+		str[i] = c;
+		i++;
+	}
+	str[i] = '\0';
+}
+
+
+/**
+ * Given a domain_name, check if the name is there in 
+ * the CSV file. If it is then output the username and 
+ * password. Otherwise, output no domain found
+ *
+ * Example of a domain name could be like Facebook, Twitter, etc
+ */
+void check_domain(char *domain_name, FILE *file) {
+
+	char domain[MAXCHARS];
+	char username[MAXCHARS];
+	char password[MAXCHARS];
+
+	int character;
+	fgets(domain, MAXCHARS, file);
+	while (1) {
+		memset(domain, '\0', MAXCHARS);
+		set_string(domain, file);
+
+		if (strcmp(domain, domain_name) == 0) {
+			set_string(username, file);
+			set_string(password, file);
+			printf("Domain name: %s\nUsername: %s\nPassword: %s\n", domain, username, password);
+			return;
+		}
+		else {
+			while ((character= fgetc(file)) != '\n' && character != EOF);
+			if (character== EOF) {
+				printf("No such domain found\n");
+				return;
+			}
+
+		}
+
+	}
+
+}
+
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		fprintf(stderr, "Usage: csv_reader file");
+		exit(1);
+	}
+	FILE *fp = fopen(argv[1], "r");
+	if (fp == NULL) {
+		perror("fopen");
+		exit(1);
+	}
+
+	char domain_name[MAXCHARS];
+	printf("Enter a record the domain that you are looking for:");	
+	scanf("%s", domain_name);
+
+	check_domain(domain_name, fp);
+	exit(0);
+}
