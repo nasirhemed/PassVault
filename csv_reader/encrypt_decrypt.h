1a9aeceb456b3224c9ccb7dfa74e2968dc45745f
Nasir Hemed <nasirhemed@ymail.com>
2018-08-29 14:41:18 -0400

    Add header file for encryption/decryption

---
+++ csv_reader/encrypt_decrypt.h
@@ -0,0 +1,18 @@
+#ifndef ENCRYPT_DECRYPT_H
+#define ENCRYPT_DECRYPT_H
+#include <wolfssl/options.h>
+#include <wolfssl/wolfcrypt/aes.h>
+#include <wolfssl/wolfcrypt/sha256.h>
+#include <wolfssl/wolfcrypt/pwdbased.h>
+
+
+#define MAXCHARS 256
+#define KEYSIZE 256
+#define SALT_SIZE 8
+
+int AesEncrypt(Aes *aes, byte *key, int size, char *input_lines, FILE *outfile);
+int AesDecrypt(Aes *aes, byte *key, int size, FILE *inFile, char **out);
+int GenerateKey(RNG *rng, byte *key, int size, byte *salt, int pad);
+int NoEcho(char* key, int size);
+int check_if_valid_csv(char *string);
+#endif
