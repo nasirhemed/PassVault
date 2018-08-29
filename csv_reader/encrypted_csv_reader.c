d2ca5aec0ddd1c264ded617e6055e34c9d1be001
Nasir Hemed <nasirhemed@ymail.com>
2018-08-29 17:15:09 -0400

    Add documentation. Wrap the stuff in main into a function

--- csv_reader/encrypted_csv_reader.c
+++ csv_reader/encrypted_csv_reader.c
@@ -13,14 +13,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <termios.h>
-#include <wolfssl/options.h>
-#include <wolfssl/wolfcrypt/aes.h>
-#include <wolfssl/wolfcrypt/sha256.h>
-#include <wolfssl/wolfcrypt/random.h>
-#include <wolfssl/wolfcrypt/pwdbased.h>
-
-#define SALT_SIZE 8
-#define MAXCHARS 256
+#include "encrypt_decrypt.h"
 
 /**
  * Given a char array and a CSV string pointer, set the string
@@ -39,111 +32,6 @@ void set_string(char password[], char **pointer_string) {
 	}
 	str[i] = '\0';
 	*pointer_string = ++p;
-}
-
-/**
- * Given a string, check if the header is of the valid CSV file:
- * 	Domain name,Username,Password
- * Return -1 if the file does not the header and 0 otherwise 
- */ 
-int check_if_valid_csv(char *string) {
-	char toCheck[] = "Domain name,Username,Password";
-	for (int i = 0; i < strlen(toCheck); i++) {
-		if (toCheck[i] != string[i]) {
-			return -1;
-		}
-	}
-	return 0;
-}
-
-
-/*
- * Given a file and its key, decrypt the file and store the contents in 
- * a string that is allocated in the heap. Make sure the string is freed 
- * (char *out). The file is assumed to be an encrypted CSV file. If the key 
- * is not correct, this function return -1. If there are any other errors, 
- * -10** is returned. If successful, this function returns 0
- */
-int AesDecrypt(Aes* aes, byte* key, int size, FILE* inFile, char **out)
-{
-	RNG     rng;
-	byte    iv[AES_BLOCK_SIZE];
-	byte*   input;
-	byte*		output = (byte *) *out;
-	byte    salt[SALT_SIZE] = {0};
-
-	int     i = 0;
-	int     ret = 0;
-	int     length;
-	int     aSize;
-
-	fseek(inFile, 0, SEEK_END);
-	length = ftell(inFile);
-	fseek(inFile, 0, SEEK_SET);
-	aSize = length;
-
-	input = malloc(aSize);
-	output = malloc(aSize);
-
-	wc_InitRng(&rng);
-
-	/* reads from inFile and wrties whatever is there to the input array */
-	ret = fread(input, 1, length, inFile);
-	if (ret == 0) {
-		printf("Input file does not exist.\n");
-		return -1010;
-	}
-	for (i = 0; i < SALT_SIZE; i++) {
-		/* finds salt from input message */
-		salt[i] = input[i];
-	}
-	for (i = SALT_SIZE; i < AES_BLOCK_SIZE + SALT_SIZE; i++) {
-		/* finds iv from input message */
-		iv[i - SALT_SIZE] = input[i];
-	}
-
-	/* replicates old key if keys match */
-	ret = wc_PBKDF2(key, key, strlen((const char*)key), salt, SALT_SIZE, 
-			4096, size, SHA256);
-	if (ret != 0)
-		return -1050;
-
-	/* sets key */
-	ret = wc_AesSetKey(aes, key, AES_BLOCK_SIZE, iv, AES_DECRYPTION);
-	if (ret != 0)
-		return -1002;
-
-	/* change length to remove salt/iv block from being decrypted */
-	length -= (AES_BLOCK_SIZE + SALT_SIZE);
-	for (i = 0; i < length; i++) {
-		/* shifts message: ignores salt/iv on message*/
-		input[i] = input[i + (AES_BLOCK_SIZE + SALT_SIZE)];
-	}
-	/* decrypts the message to output based on input length + padding*/
-	ret = wc_AesCbcDecrypt(aes, output, input, length);
-	if (ret != 0)
-		return -1006;
-
-	if (salt[0] != 0) {
-		/* reduces length based on number of padded elements */
-		length -= output[length-1];
-	}
-	output[length - 1] = '\0';
-	printf("%s\n", (unsigned char *)output);
-	if (check_if_valid_csv((char *)output) == -1) {
-		return -1;
-	}
-	*out = (char *)output;
-
-	/* closes the opened files and frees the memory*/
-	memset(input, 0, aSize);
-	memset(key, 0, size);
-	free(input);
-	free(key);
-	fclose(inFile);
-	wc_FreeRng(&rng);
-
-	return 0;
 }
 
 
@@ -187,36 +75,27 @@ void check_domain(char *domain_name, char *csv_string) {
 	}
 
 }
+/**
+	* This function reads inputs from stdin and prints
+	* whether the record is in the string out
+	*/
+void read_inputs(char *domain_name, char *out) {
 
-/*
- * Temporarily disable displaying the characters on the terminal to allow 
- * the user to securely enter the password for decrypting the file.
- */
-int NoEcho(char* key, int size)
-{
-	struct termios oflags, nflags;
+	while (1) {
 
-	/* disabling echo */
-	tcgetattr(fileno(stdin), &oflags);
-	nflags = oflags;
-	nflags.c_lflag &= ~ECHO;
-	nflags.c_lflag |= ECHONL;
+		printf("Enter a record the domain that you are looking for:"
+				"(Press Enter or EOF if you want to skip): "
+		      );
 
-	if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0) {
-		printf("Error\n");
-		return -1060;
+		if (fgets(domain_name, MAXCHARS, stdin) == NULL ||\
+				domain_name[0] == '\n'){
+			printf("\n");
+			break;
+		}
+		domain_name[strlen(domain_name) - 1] = '\0';
+		printf("%s\n", domain_name);
+		check_domain(domain_name, out);
 	}
-
-	printf("Unique Password: ");
-	fgets(key, size, stdin);
-	key[strlen(key) - 1] = 0;
-	printf("\n");
-	/* restore terminal */
-	if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0) {
-		printf("Error\n");
-		return -1070;
-	}
-	return 0;
 }
 
 int main(int argc, char **argv) {
@@ -231,21 +110,15 @@ int main(int argc, char **argv) {
 	}
 	Aes aes;
 	char *domain_name = malloc(MAXCHARS);
-	char *pass = malloc(256);
+	char *pass = malloc(KEYSIZE);
 	char *out;
 
-	NoEcho(pass, 256);
-	if (AesDecrypt(&aes,(byte *) pass, 256, fp, &out) == -1) {
+	NoEcho(pass, KEYSIZE);
+	if (AesDecrypt(&aes,(byte *) pass, KEYSIZE, fp, &out) == -1) {
 		fprintf(stderr, "Invalid Password\n");
 		exit(1);
 	}
-	while (1) {
-
-		printf("Enter a record the domain that you are looking for:");
-		fgets(domain_name, MAXCHARS, stdin);
-		domain_name[strlen(domain_name) - 1] = '\0';
-		check_domain(domain_name, out);
-	}
+	read_inputs(domain_name, out);
 	free(out);
 	exit(0);
 }
