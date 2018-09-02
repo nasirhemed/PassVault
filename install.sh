64f4c90f816b5dc89d57f8915896aefd8f8d2d46
Nasir Hemed <nasirhemed@ymail.com>
2018-09-02 11:33:38 -0400

    Add initial installation file

---
+++ install.sh
@@ -0,0 +1,48 @@
+currDir="$(pwd)"
+
+if ! dpkg -s libbsd-dev > /dev/null || ! dpkg -s git > /dev/null
+then
+	echo Please install libssd-dev before running this script
+	exit 1
+fi
+
+echo "Enter the path directory to store your encrypted passwords"
+echo "Note: If nothing is specified, a default path (~/passFiles) will be "\
+	"created"
+read PassFolders
+if test -z $PassFolders
+then
+	if ! mkdir ~/passFiles/ 2> /dev/null; then
+		echo "Folder already exists. Would you like to continue? (Y/N)"
+		read y
+		if test $y != Y && test $y != y
+		then
+			echo "Cancelling installation"
+			exit 1
+		fi
+
+	fi
+	PassFolders=~/passFiles/	
+fi
+echo $PassFolders
+if test -e $PassFolders
+then
+	echo excellent > /dev/null/
+else
+	echo "Directory not found. Creating directory"
+	mkdir "$PassFolders"
+	cd "$PassFolders"
+	echo $(pwd)
+	PassFolders=$(pwd)
+	cd "$currDir"
+fi
+echo $PassFolders
+
+export PassFolders
+git clone https://github.com/wolfSSL/wolfssl.git
+cd wolfssl
+./configure --enable-pwdbased --enable-dtls
+make
+make install
+cd ..
+
